569813409de3f68359dc4759dc6df256a66ddcd0
alphaOri <david.everett.smith@gmail.com>
2020-03-12 13:22:23 -0400

    updated displayValue to enable blinking

--- src/scenes/Dashboard/DisplayValue.js
+++ src/scenes/Dashboard/DisplayValue.js
@@ -10,37 +10,37 @@ export class DisplayValueSeparateUnits extends Component {
   }
 
   shouldComponentUpdate(nextProps){
-  	if(nextProps.value == null){
-  		//console.log("DisplayValueSeparateUnits:shouldComponentUpdate:return false")
-  		return false;
-  	}
-  	//console.log("DisplayValueSeparateUnits:shouldComponentUpdate:return true")
-	return true;
+    if(nextProps.value == null){
+      //console.log("DisplayValueSeparateUnits:shouldComponentUpdate:return false")
+      return false;
+    }
+    //console.log("DisplayValueSeparateUnits:shouldComponentUpdate:return true")
+   return true;
   }
 
   render() {
-  	//console.log("DisplayValueSeparateUnits.js:render(): ")
-  	//console.log(this.props)
+    //console.log("DisplayValueSeparateUnits.js:render(): ")
+    //console.log(this.props)
 
-  	if(this.props.value === null || this.props.value === undefined){
-		return (
-			<React.Fragment>
-				---
-				<div class="CardBodyUnits">
-					{' '}{this.props.units}
-				</div>
-			</React.Fragment>
-		); 
-	}
+    if(this.props.value === null || this.props.value === undefined){
+      return (
+        <React.Fragment>
+          ---
+          <div class="CardBodyUnits">
+            {' '}{this.props.units}
+          </div>
+        </React.Fragment>
+      ); 
+    }
 
-	return (
-		<React.Fragment>
-			{this.props.value}
-			<div class="CardBodyUnits">
-				{' '}{this.props.units}
-			</div>
-		</React.Fragment>
-	);
+    return (
+      <React.Fragment>
+        {this.props.value}
+        <div class="CardBodyUnits">
+          {' '}{this.props.units}
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
@@ -52,31 +52,34 @@ export class DisplayValueAndUnits extends Component {
   }
 
   shouldComponentUpdate(nextProps){
-  	if(nextProps.value == null){
-  		//console.log("DisplayValueAndUnits:shouldComponentUpdate:return false")
-  		return false;
-  	}
-  	//console.log("DisplayValueAndUnits:shouldComponentUpdate:return true")
-	return true;
+    if(nextProps.value == null){
+      //console.log("DisplayValueAndUnits:shouldComponentUpdate:return false")
+      return false;
+    }
+    //console.log("DisplayValueAndUnits:shouldComponentUpdate:return true")
+    return true;
   }
 
   render() {
-  	//console.log("DisplayValueAndUnits.js:render(): ")
-  	//console.log(this.props)
+    console.log("DisplayValueAndUnits.js:render(): ")
+    console.log(this.props)
 
-  	if(this.props.value === null || this.props.value === undefined){
-		return (
-			<React.Fragment>
-				--{this.props.units}
-			</React.Fragment>
-		); 
-	}
+    if(!this.props.disable) {
+      if(this.props.value === null || this.props.value === undefined){
+        return (
+          <React.Fragment>
+            --{this.props.units}
+          </React.Fragment>
+        ); 
+      }
 
-	return (
-		<React.Fragment>
-			{this.props.value}{this.props.units}
-		</React.Fragment>
-	);
+      return (
+        <React.Fragment>
+          {this.props.value}{this.props.units}
+        </React.Fragment>
+      );
+    }
+    return (null);
   }
 }
 
@@ -88,40 +91,40 @@ class DisplayColumn extends Component {
   }
 
   shouldComponentUpdate(nextProps){
-  	if(nextProps.value == null){
-  		//console.log("DisplayColumn:shouldComponentUpdate:return false")
-  		return false;
-  	}
-  	//console.log("DisplayColumn:shouldComponentUpdate:return true")
-	return true;
+    if(nextProps.value == null){
+      //console.log("DisplayColumn:shouldComponentUpdate:return false")
+      return false;
+    }
+    //console.log("DisplayColumn:shouldComponentUpdate:return true")
+    return true;
   }
 
   render() {
-  	//console.log("DisplayColumn.js:render(): ")
-  	//console.log(this.props)
+    //console.log("DisplayColumn.js:render(): ")
+    //console.log(this.props)
 
-  	if(this.props.value === null || this.props.value === undefined){
-		return (
-	        <div class="DisplayColumn">
-	            <div class="CardBodyText">
-	                {this.props.label}
-	            </div>
-	            <div class="ValueDisplay">
-	                ---
-	            </div>
-	        </div>
-		); 
-	}
-
-	return (
+    if(this.props.value === null || this.props.value === undefined){
+      return (
         <div class="DisplayColumn">
-            <div class="CardBodyText">
-                {this.props.label}
-            </div>
-            <div class="ValueDisplay">
-                <DisplayValueAndUnits value={this.props.value} units={this.props.units}/>
-            </div>
+          <div class="CardBodyText">
+              {this.props.label}
+          </div>
+          <div class="ValueDisplay">
+              ---
+          </div>
         </div>
-	);
+      ); 
+    }
+
+    return (
+      <div class="DisplayColumn">
+          <div class="CardBodyText">
+              {this.props.label}
+          </div>
+          <div class="ValueDisplay">
+              <DisplayValueAndUnits value={this.props.value} units={this.props.units}/>
+          </div>
+      </div>
+    );
   }
 }
